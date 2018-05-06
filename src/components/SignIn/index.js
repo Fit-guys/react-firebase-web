96422974a54aab277f654657770845f66a2f7e7f
AlexandrDobrovolsy <sasha732sasha000@gmail.com>
2018-05-06 05:21:09 +0300

    added login page protothype

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -7,6 +7,13 @@ import { auth } from '../../firebase';
 import * as routes from '../../constants/routes';
 import firebase from 'firebase';
 import Button from "react-bootstrap/es/Button";
+import FormGroup from "react-bootstrap/es/FormGroup";
+import Col from "react-bootstrap/es/Col";
+import FormControl from "react-bootstrap/es/FormControl";
+import Form from "react-bootstrap/es/Form";
+import Panel from "react-bootstrap/es/Panel";
+import './login.css';
+import Row from "react-bootstrap/es/Row";
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
@@ -20,10 +27,7 @@ provider.setCustomParameters({
 
 const SignInPage = ({ history }) =>
   <div>
-    <h1>SignIn</h1>
     <SignInForm history={history} />
-    <PasswordForgetLink />
-    <SignUpLink />
   </div>;
 
 const updateByPropertyName = (propertyName, value) => () => ({
@@ -90,29 +94,64 @@ class SignInForm extends Component {
       email === '';
 
     return (
-      <div>
-          <form onSubmit={this.onSubmit}>
-              <input
-                  value={email}
-                  onChange={event => this.setState(updateByPropertyName('email', event.target.value))}
-                  type="text"
-                  placeholder="Email Address"
-              />
-              <input
-                  value={password}
-                  onChange={event => this.setState(updateByPropertyName('password', event.target.value))}
-                  type="password"
-                  placeholder="Password"
-              />
-              <button disabled={isInvalid} type="submit">
-                  Sign In
-              </button>
+      <div id={'panel-container'}>
+          <Panel id={'form-panel'} bsStyle="primary">
+              <Panel.Heading>
+                  <Panel.Title componentClass="h3">Sign In</Panel.Title>
+              </Panel.Heading>
+              <Row id={'panel-content-container'}>
+                  <Form onSubmit={this.onSubmit} horizontal>
+                      <div id={'provider-buttons'}>
+                          <Button onClick={this.withGoogle} id={"google"}>
+                              Goolge
+                          </Button>
+                          <Button onClick={this.withGoogle} id={'facebook'}>
+                              Facebook
+                          </Button>
+                          <Button onClick={this.withGoogle} id={'github'}>
+                              Github
+                          </Button>
+                      </div>
+                      <FormGroup controlId="formHorizontalEmail">
+                          <Col sm={2} id={'email-caption'}>
+                              Email
+                          </Col>
+                          <Col sm={10}>
+                              <FormControl
+                                  value={email}
+                                  onChange={event => this.setState(updateByPropertyName('email', event.target.value))}
+                                  type="text"
+                                  placeholder="Email Address"
+                              />
+                          </Col>
+                      </FormGroup>
 
-              { error && <p>{error.message}</p> }
-          </form>
-          <Button onClick={this.withGoogle}>
-              Ggl
-          </Button>
+                      <FormGroup controlId="formHorizontalPassword">
+                          <Col sm={2} id={'password-caption'}>
+                              Password
+                          </Col>
+                          <Col sm={10}>
+                              <FormControl
+                                  value={password}
+                                  onChange={event => this.setState(updateByPropertyName('password', event.target.value))}
+                                  type="password"
+                                  placeholder="Password"
+                              />
+                          </Col>
+                      </FormGroup>
+
+                      <FormGroup>
+                          <Col smOffset={2} sm={10}>
+                              <Button disabled={isInvalid} type="submit" bsStyle="primary">Sign in</Button>
+                          </Col>
+                      </FormGroup>
+
+                      { error && <p id={'error-message'}>{error.message}</p> }
+                      <PasswordForgetLink />
+                      <SignUpLink />
+                  </Form>
+              </Row>
+          </Panel>
       </div>
     );
   }
